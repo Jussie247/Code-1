6724ba808f565681dbf47696fb30e46f478a8be3
Jussie247 <justus.schultzke@gmail.com>
2024-06-27 09:31:20 +0200

    Added BMC Page

---
+++ Tasks/L05/Blackmail_Companion/BMC.js
@@ -0,0 +1,33 @@
+"use strict";
+var L05_Blackmail_Companion;
+(function (L05_Blackmail_Companion) {
+    console.log("start");
+    let chosenCharacter = "A";
+    window.addEventListener("load", handleLoad);
+    function handleLoad(_event) {
+        let mail = document.querySelector("div#mail");
+        mail.addEventListener("click", placeLetter);
+        document.addEventListener("keydown", chooseCharacter);
+    }
+    function placeLetter(_event) {
+        // console.log(_event);
+        let x = _event.offsetX;
+        let y = _event.offsetY;
+        let mail = _event.target;
+        let letter = document.createElement("span");
+        mail.appendChild(letter);
+        letter.textContent = chosenCharacter;
+        letter.style.left = x + "px";
+        letter.style.top = y + "px";
+        letter.addEventListener("click", deleteLetter);
+    }
+    function chooseCharacter(_event) {
+        //console.log(_event)
+        chosenCharacter = _event.key;
+    }
+    function deleteLetter(_event) {
+        let target = _event.target;
+        let parent = target.parentNode;
+        parent.removeChild(target);
+    }
+})(L05_Blackmail_Companion || (L05_Blackmail_Companion = {}));
