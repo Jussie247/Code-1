f1e076bf59c9d8b22e75ebc354f06147e2f782e6
Jussie247 <justus.schultzke@gmail.com>
2024-07-01 10:28:03 +0200

    Updated "" in canvas. Added Coordinate System to VGP

--- Tasks/L06/VectorGraphics/VectorGP.js
+++ Tasks/L06/VectorGraphics/VectorGP.js
@@ -1,1 +1,24 @@
 "use strict";
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+if (ctx) {
+    // Draw the coordinate system 
+    function drawCoordinateSystem(ctx, originX, originY, notchDistance, axisLength) {
+        // Draw x and Y axes 
+        ctx.moveTo(originX, originY);
+        ctx.lineTo(originX + axisLength, originY);
+        ctx.moveTo(originX, originY);
+        ctx.lineTo(originX, originY + axisLength);
+        ctx.stroke();
+        // Draw notcheson X and Y axes 
+        for (let i = 0; i <= axisLength; i += notchDistance) {
+            ctx.moveTo(originX + i, originY - 5);
+            ctx.lineTo(originX + i, originY + 5);
+            ctx.moveTo(originX - 5, originY + i);
+            ctx.lineTo(originX + 5, originY + i);
+            ctx.stroke();
+        }
+    }
+    // Draw the coordinate sytsem at (0,0)
+    drawCoordinateSystem(ctx, 0, 0, 10, 200);
+}
--- Tasks/L06/VectorGraphics/VectorGP.ts
+++ Tasks/L06/VectorGraphics/VectorGP.ts
@@ -1,3 +1,25 @@
-const canvas = document.getElementById("canvs") as HTMLCanvasElement;
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d");
 
+if (ctx) {
+    // Draw the coordinate system 
+    function drawCoordinateSystem(ctx: CanvasRenderingContext2D, originX: number, originY: number, notchDistance: number, axisLength: number){
+        // Draw x and Y axes 
+        ctx.moveTo(originX, originY);
+        ctx.lineTo(originX + axisLength, originY);
+        ctx.moveTo(originX, originY);
+        ctx.lineTo(originX, originY + axisLength);
+        ctx.stroke();
+
+        // Draw notcheson X and Y axes 
+        for (let i = 0; i<= axisLength; i+= notchDistance) {
+            ctx.moveTo(originX + i, originY - 5);
+            ctx.lineTo(originX + i, originY + 5);
+            ctx.moveTo(originX - 5, originY + i);
+            ctx.lineTo(originX + 5, originY + i);
+            ctx.stroke();
+        }
+    }
+    // Draw the coordinate sytsem at (0,0)
+    drawCoordinateSystem(ctx, 0, 0, 10, 200);
+}
