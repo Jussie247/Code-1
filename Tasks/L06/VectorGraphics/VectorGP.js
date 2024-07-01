5ae163d9ce88cf73119fc2a2db88a914f4831461
Jussie247 <justus.schultzke@gmail.com>
2024-07-01 12:08:40 +0200

    Created Rotation loop

--- Tasks/L06/VectorGraphics/VectorGP.js
+++ Tasks/L06/VectorGraphics/VectorGP.js
@@ -21,4 +21,116 @@ if (ctx) {
     }
     // Draw the coordinate sytsem at (0,0)
     drawCoordinateSystem(ctx, 0, 0, 10, 200);
+    ctx.beginPath();
+    ctx.translate(300, 300);
+    ctx.scale(1, 1);
+    for (let i = 0; i < 50; i++) {
+        ctx.fillStyle = ctx.fillStyle = `hsl(${Math.random() * 360}, 50%, 50%)`;
+        ctx.fillRect(200, 50, 50, 100);
+        ctx.rotate(25 * Math.PI / 180);
+    }
+    ctx.beginPath();
+    ctx.translate(0, 0);
+    ctx.scale(0.75, 0.75);
+    for (let i = 0; i < 50; i++) {
+        ctx.fillStyle = ctx.fillStyle = `hsl(${Math.random() * 360}, 50%, 50%)`;
+        ctx.fillRect(200, 50, 50, 100);
+        ctx.rotate(25 * Math.PI / 180);
+    }
+    ctx.beginPath();
+    ctx.translate(0, 0);
+    ctx.scale(0.75, 0.75);
+    for (let i = 0; i < 50; i++) {
+        ctx.fillStyle = ctx.fillStyle = `hsl(${Math.random() * 360}, 50%, 50%)`;
+        ctx.fillRect(200, 50, 50, 100);
+        ctx.rotate(25 * Math.PI / 180);
+    }
+    ctx.beginPath();
+    ctx.translate(0, 0);
+    ctx.scale(0.75, 0.75);
+    for (let i = 0; i < 50; i++) {
+        ctx.fillStyle = ctx.fillStyle = `hsl(${Math.random() * 360}, 50%, 50%)`;
+        ctx.fillRect(200, 50, 50, 100);
+        ctx.rotate(25 * Math.PI / 180);
+    }
+    ctx.beginPath();
+    ctx.translate(0, 0);
+    ctx.scale(0.75, 0.75);
+    for (let i = 0; i < 50; i++) {
+        ctx.fillStyle = ctx.fillStyle = `hsl(${Math.random() * 360}, 50%, 50%)`;
+        ctx.fillRect(200, 50, 50, 100);
+        ctx.rotate(25 * Math.PI / 180);
+    }
+    ctx.beginPath();
+    ctx.translate(0, 0);
+    ctx.scale(0.75, 0.75);
+    for (let i = 0; i < 50; i++) {
+        ctx.fillStyle = ctx.fillStyle = `hsl(${Math.random() * 360}, 50%, 50%)`;
+        ctx.fillRect(200, 50, 50, 100);
+        ctx.rotate(25 * Math.PI / 180);
+    }
+    ctx.beginPath();
+    ctx.translate(0, 0);
+    ctx.scale(0.75, 0.75);
+    for (let i = 0; i < 50; i++) {
+        ctx.fillStyle = ctx.fillStyle = `hsl(${Math.random() * 360}, 50%, 50%)`;
+        ctx.fillRect(200, 50, 50, 100);
+        ctx.rotate(25 * Math.PI / 180);
+    }
+    ctx.beginPath();
+    ctx.translate(0, 0);
+    ctx.scale(0.75, 0.75);
+    for (let i = 0; i < 50; i++) {
+        ctx.fillStyle = ctx.fillStyle = `hsl(${Math.random() * 360}, 50%, 50%)`;
+        ctx.fillRect(200, 50, 50, 100);
+        ctx.rotate(25 * Math.PI / 180);
+    }
+    ctx.beginPath();
+    ctx.translate(0, 0);
+    ctx.scale(0.75, 0.75);
+    for (let i = 0; i < 50; i++) {
+        ctx.fillStyle = ctx.fillStyle = `hsl(${Math.random() * 360}, 50%, 50%)`;
+        ctx.fillRect(200, 50, 50, 100);
+        ctx.rotate(25 * Math.PI / 180);
+    }
+    ctx.beginPath();
+    ctx.translate(0, 0);
+    ctx.scale(0.75, 0.75);
+    for (let i = 0; i < 50; i++) {
+        ctx.fillStyle = ctx.fillStyle = `hsl(${Math.random() * 360}, 50%, 50%)`;
+        ctx.fillRect(200, 50, 50, 100);
+        ctx.rotate(25 * Math.PI / 180);
+    }
+    ctx.beginPath();
+    ctx.translate(0, 0);
+    ctx.scale(0.75, 0.75);
+    for (let i = 0; i < 50; i++) {
+        ctx.fillStyle = ctx.fillStyle = `hsl(${Math.random() * 360}, 50%, 50%)`;
+        ctx.fillRect(200, 50, 50, 100);
+        ctx.rotate(25 * Math.PI / 180);
+    }
+    ctx.beginPath();
+    ctx.translate(0, 0);
+    ctx.scale(0.75, 0.75);
+    for (let i = 0; i < 50; i++) {
+        ctx.fillStyle = ctx.fillStyle = `hsl(${Math.random() * 360}, 50%, 50%)`;
+        ctx.fillRect(200, 50, 50, 100);
+        ctx.rotate(25 * Math.PI / 180);
+    }
+    ctx.beginPath();
+    ctx.translate(0, 0);
+    ctx.scale(0.75, 0.75);
+    for (let i = 0; i < 50; i++) {
+        ctx.fillStyle = ctx.fillStyle = `hsl(${Math.random() * 360}, 50%, 50%)`;
+        ctx.fillRect(200, 50, 50, 100);
+        ctx.rotate(25 * Math.PI / 180);
+    }
+    ctx.beginPath();
+    ctx.translate(0, 0);
+    ctx.scale(0.75, 0.75);
+    for (let i = 0; i < 50; i++) {
+        ctx.fillStyle = ctx.fillStyle = `hsl(${Math.random() * 360}, 50%, 50%)`;
+        ctx.fillRect(200, 50, 50, 100);
+        ctx.rotate(25 * Math.PI / 180);
+    }
 }
