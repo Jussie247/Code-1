3dbe34065c0ba6f649545969296b83d039301a72
“Jussie247” <justus.schultzke@gmail.com>
2024-07-09 19:30:22 +0200

    Added songs

--- Test/test.ts
+++ Test/test.ts
@@ -1,109 +1,19 @@
-document.getElementById("start-game")!.addEventListener("click", startGame);
+let song1: string = "11556654433221"; //Twinkle Twinkle
+let song2: string = "321233322233355321233322232"; // Mary Had a Little Lamb
+let song3: string = "112143254321145321143543211"; // Happy Birthday
+let song4: string = "334556654433221123322344433223322344433322113221"; // Ode to Joy
+let song5: string = "333344322233334433223433322"; // Jingle Bells
+let song6Refrain: string = "3333555333335532"; // We will rock you Queen
+let song8Refrain: string = "5555335566655433321"; // Let it be - Beatels
+let song9Refrain: string = "1122333445566554"; // old town road - lil nas x
+let song10Refrain: string = "556655556644554"; // Believer - Imagine Dragons
+let song11Refrain: string = "554433224455665"; // Cant stop the feeling - Justin Timberlake
+let song12Refrain: string = "3344555666655543"; // Bad romance lady gaga
+let song13Refrain: string = "5544332244556677"; // Fireworks - Katy Perry
 
-function startGame() {
-    // Clear the body
-    document.body.innerHTML = "";
 
-    // Create and append the canvas element
-    const canvas = document.createElement("canvas");
-    canvas.id = "canvas";
-    canvas.width = 1200;
-    canvas.height = 600;
-    canvas.style.border = "1px solid black";
-    document.body.appendChild(canvas);
 
-    // Initialize the xylophone game
-    initXylophoneGame();
-}
+ 
+ 
 
-function initXylophoneGame() {
-    const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-    const ctx = canvas.getContext("2d")!;
 
-    const keyCount = 8;
-    let pitches: string[] = ["C", "D", "E", "F", "G", "A", "H", "C'"];
-    let sounds: string[] = ["xylophone-c3.wav", "xylophone-d3.wav", "xylophone-e3.wav", "xylophone-f3.wav",
-                            "xylophone-g3.wav", "xylophone-a.wav", "xylophone-b-h.wav", "xylophone-c2_kleines_C.wav"];
-    let colors: string[] = ["#ea4029", "#2020b8", "#f3f646", "#42f4e9", "#53ed41", "#b53af3", "#f0af37", "#f360c0"];
-    let keys: XyloKey[] = [];
-
-    const audioCtx = new AudioContext();
-
-    canvas.addEventListener("click", handleClick);
-    createBoard();
-
-    interface XyloKey {
-        sound: string,
-        pitch: string,
-        width: number,
-        length: number,
-        color: string,
-        posX: number,
-        posY: number,
-        path: Path2D,
-    }
-
-    function createBoard() {
-        let rods: Path2D = new Path2D;
-        ctx.strokeStyle = "black";
-        ctx.lineWidth = 3;
-
-        rods.moveTo(120, 200);
-        rods.lineTo(canvas.width - 150, 200);
-        rods.moveTo(120, canvas.height - 200);
-        rods.lineTo(canvas.width - 150, canvas.height - 200);
-
-        ctx.stroke(rods);
-
-        for (let i: number = 0; i < keyCount; i++) {
-            let y: number = 20 + 20 * i;
-            let x: number = 150 + 110 * i;
-            let l: number = 560 - 40 * i;
-
-            let newKey: XyloKey = {
-                sound: sounds[i],
-                pitch: pitches[i],
-                width: 100,
-                length: l,
-                color: colors[i],
-                posX: x,
-                posY: y,
-                path: new Path2D,
-            }
-
-            keys.push(newKey);
-            drawKey(newKey);
-        }
-    }
-
-    function drawKey(_key: XyloKey) {
-        let x: number = _key.posX;
-        let y: number = _key.posY;
-        let keyPath: Path2D = _key.path;
-
-        ctx.fillStyle = _key.color;
-        ctx.lineWidth = 1;
-
-        keyPath.moveTo(x, y);
-        keyPath.rect(x, y, _key.width, _key.length);
-        ctx.fill(keyPath);
-        ctx.stroke(keyPath);
-    }
-
-    function handleClick(_event: MouseEvent) {
-        let x: number = _event.offsetX;
-        let y: number = _event.offsetY;
-
-        for (let i: number = 0; i < keys.length; i++) {
-            let keyCheck: XyloKey = keys[i];
-            let sound = new Audio(keyCheck.sound);
-
-            if (ctx.isPointInPath(keyCheck.path, x, y)) {
-                console.log(keyCheck.pitch);
-                audioCtx.resume().then(() => {
-                    sound.play();
-                });
-            }
-        }
-    }
-}
